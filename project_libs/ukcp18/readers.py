c4665473ea8d8032efb9ae387956793785a5d499
antony-wilson <antoyn.wilson@stfc.ac.uk>
2017-11-24 14:58:54 +0000

    update data source names

--- project_libs/ukcp18/readers.py
+++ project_libs/ukcp18/readers.py
@@ -1,7 +1,7 @@
 import iris
 
 import glob 
-files = glob.glob("fakedata/ukcp18/data/land_probabilistic/25km/a1b/percentile/pr/v20170331/pr*.nc")
+files = glob.glob("fakedata/ukcp18/data/land-prob/25km/a1b/percentile/pr/v20170331/pr*.nc")
 
 cubes = iris.load(files, ["precipitation rate"])
 cube = cubes.concatenate_cube()
